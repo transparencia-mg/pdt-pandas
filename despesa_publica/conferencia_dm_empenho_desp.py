f91daa0b38c3d647284cc06a771c5588c2658047
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-04-12 20:21:30 -0300

    Improve code using just ckan package_show url

--- despesa_publica/conferencia_dm_empenho_desp.py
+++ despesa_publica/conferencia_dm_empenho_desp.py
@@ -4,19 +4,14 @@ import pandas as pd
 
 despesas_dataset = requests.get('https://dados.mg.gov.br/api/3/action/package_show?id=despesa')
 despesas_dataset = despesas_dataset.json()
-despesas_dict = despesas_dataset['result']
-despesas_id = despesas_dict['id']
-resources = [x['value'] for x in despesas_dict['extras'] if x['key'] == 'resources_ids'][0]
-resources = json.loads(resources)
-dm_empenhos = [x for x in resources if x.startswith('dm_empenho_desp')]
+resources = despesas_dataset['result']['resources']
+dm_empenhos = [x for x in resources if x['url'].split('/')[-1].startswith('dm_empenho_desp')]
 
 df = []
 for resource in dm_empenhos:
-	resouce_id = resources[resource]
-	resource_url = f'https://dados.mg.gov.br/dataset/{despesas_id}/resource/{resouce_id}/download/{resource}.csv.gz'
-	resource_df = pd.read_csv(resource_url, delimiter=";")
+	resource_df = pd.read_csv(resource['url'], delimiter=";")
 	df.append(resource_df)
-	print(f'{resource} data frame created.')	
+	print(f'{resource["name"]} data frame created.')	
 df = pd.concat(df)
 print(f'Concat data frames')
 grouped = df.groupby('ano_exercicio')
